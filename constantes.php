b8018fdcf393598a8ce2b700eb06dd6eedcc8446
Traian28 <traian.alimentibel@gmail.com>
2019-04-21 18:42:07 -0300

    Add files via upload

---
+++ constantes.php
@@ -0,0 +1,13 @@
+<?php
+
+define ('PI', 3.14);
+define ('NOMBRE', 'traian');
+
+#en variables podes establecer el nombre y modificarlo, en constantes no.
+
+$nombre = "carlos";
+$nombre = "juan";
+
+echo NOMBRE;
+
+?>
